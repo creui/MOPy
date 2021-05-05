bdac9de097e4b8c9d5a9292bde774600b352e78e
Leochares <5h17f4c3@gmail.com>
2021-05-05 15:36:50 +0800

    Added a list feature
    MOPy now asks for a list, sorted or unsorted, it will sort the list and tell you what the value of the M.O.P. is in the list.

--- mo.py
+++ mo.py
@@ -1,12 +1,18 @@
 P = float(input('\nP')) / 100
 N = int(input('N = ')) + 1
 
+list_input = [int(i) for i in input(
+    '\nEnter the values (separated by spaces):\n\n').split()]
+list_input.sort()
+
 result = P*N
 rounded = round(result + .1)
 
-if str(result)[-1] == '5':
-	print(f'\n{result} or {rounded}. The answer is in position {rounded}.')
+if str(result)[-1] != '0':
+    print(
+        f'\nThe answer is in position {result} or {rounded}, which has a value of {list_input[rounded-1]}.\n')
 else:
-	print(f'\n{int(result)}. The answer is in position {rounded}.')
+    print(
+        f'\nThe answer is in position {rounded}, which has a value of {list_input[rounded-1]}.\n')
 
-print('\n(Make sure to arrange the values first before finding the answer.)\n')
+# print(f'\nThe value of position {rounded} is', list_input[rounded-1], '\n')
